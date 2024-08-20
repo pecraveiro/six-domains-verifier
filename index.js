37f1327665df7cca5576dacc6d77486173d62c5a
Pedro Craveiro <79882049+pecraveiro@users.noreply.github.com>
2024-08-20 14:52:40 -0400

    Updated domains

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const validDomains = [
     "queroeternavita.fun",
     "eternavita.com.br",
     "eternavita.com",
+    "oternavita.com.br",
     "omemorimax.com",
     "memorimaxoficial.fun",
     "memorimax.shop",
